7d474dfcadd80f574ce39bdefb4cd6827d4d52fc
haeke <riverae2@student.wpunj.edu>
2019-05-24 16:31:33 -0400

    added padding and space between the header title and switch button.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,7 +5,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="headerContainer">
-      <h1>Where in the world?</h1>
+      <h1 className="headerTitle">Where in the world?</h1>
       <input type="checkbox" id="toggle" className="offscreen" />{" "}
       <label for="toggle" className="switch" />
     </header>
