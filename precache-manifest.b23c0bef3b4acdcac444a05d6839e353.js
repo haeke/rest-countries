af012bfb811ba2f4d52d32a64cc9adb68263d93e
Edwin Rivera <waehake@gmail.com>
2019-06-06 09:01:21 -0400

    Updates

---
+++ precache-manifest.b23c0bef3b4acdcac444a05d6839e353.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c59a298f773429f7c95c97b77e97845b",
+    "url": "/rest-countries/index.html"
+  },
+  {
+    "revision": "8e9078d28bbf2774f2b9",
+    "url": "/rest-countries/static/css/main.6b3a4eff.chunk.css"
+  },
+  {
+    "revision": "ed93b5b9e9ab27abba12",
+    "url": "/rest-countries/static/js/2.94e5040c.chunk.js"
+  },
+  {
+    "revision": "8e9078d28bbf2774f2b9",
+    "url": "/rest-countries/static/js/main.c8850aa4.chunk.js"
+  },
+  {
+    "revision": "c5dc570e91023d6fd4b1",
+    "url": "/rest-countries/static/js/runtime~main.3e1474be.js"
+  }
+]);
